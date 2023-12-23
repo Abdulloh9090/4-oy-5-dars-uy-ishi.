d9902cf591ef0ad4ac573ceb2b91b124d8d5d438
Abdulloh9090 <152011956+Abdulloh9090@users.noreply.github.com>
2023-12-23 18:24:45 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let array = ["Abdulloh", "Mirasqarov", "92 maktab", "Front 815"]
+let array2 = ["Abdulloh", "Mirasqarov", "92 maktab", "Front 815"]
+let array3 = ["Abdulloh", "Mirasqarov", "92 maktab", "Front 815"]
+
+
+   console.log(array);
+   array2.push("Nuriddin")
+   array3.pop("Nuriddin")
+    console.log(array2);
+    console.log(array3);
+
+
+  
+
